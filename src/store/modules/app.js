2ad7a7e0df821cb1ac18325967b11bd594e06070
AngusWG <z740713651@outlook.com>
2022-06-16 18:50:50 +0800

    feat: 登录超时

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,29 +1,46 @@
-import { login as loginAPI } from '@/apis/login'
+import { login as loginApi } from '@/apis/login'
 import router from '@/router'
-
+import { setTokenTime } from '@/utils/auth'
 export default {
     namespaced: true,
     state: () => ({
-        token: localStorage.getItem('token') || ''
+        token: localStorage.getItem('token') || '',
+        siderType: true,
+        lang: localStorage.getItem('lang') || 'zh'
     }),
     mutations: {
         setToken(state, token) {
             state.token = token
             localStorage.setItem('token', token)
+        },
+        changeSiderType(state) {
+            state.siderType = !state.siderType
+        },
+        changLang(state, lang) {
+            state.lang = lang
         }
     },
     actions: {
         login({ commit }, userInfo) {
             return new Promise((resolve, reject) => {
-                loginAPI(userInfo)
-                    .then(response => {
-                        console.log(response)
-                        commit('setToken', response.token)
+                loginApi(userInfo)
+                    .then((res) => {
+                        console.log(res)
+                        commit('setToken', res.token)
+                        setTokenTime()
                         router.replace('/')
                         resolve()
                     })
-                    .catch(error => { reject(error) })
+                    .catch((err) => {
+                        reject(err)
+                    })
             })
+        },
+        // 退出
+        logout({ commit }) {
+            commit('setToken', '')
+            localStorage.clear()
+            router.replace('/login')
         }
     }
 }
