246c0d78917e634f9c21c4ac85f249df903f444f
AngusWG <z740713651@outlook.com>
2022-06-17 19:12:05 +0800

    feat: 用户增加和用户修改

--- src/apis/users.js
+++ src/apis/users.js
@@ -6,3 +6,32 @@ export const getUser = (params) => {
         params
     })
 }
+export const changeUserState = (uid, type) => {
+    return request({
+        url: `users/${uid}/state/${type}`,
+        method: 'put'
+    })
+}
+
+export const addUser = (data) => {
+    return request({
+        url: 'users',
+        method: 'POST',
+        data
+    })
+}
+
+export const editUser = (data) => {
+    return request({
+        url: `users/${data.id}`,
+        method: 'put',
+        data
+    })
+}
+
+export const deleteUser = (id) => {
+    return request({
+        url: `users/${id}`,
+        method: 'delete'
+    })
+}
--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -8,10 +8,12 @@
           v-model="queryForm"
         ></el-input>
       </el-col>
-      <el-button type="primary" :icon="Search" @click="initGetUsersList()">
+      <el-button type="primary" :icon="Search" @click="initGetUsersList">
         {{ $t('table.search') }}
       </el-button>
-      <el-button type="primary">{{ $t('table.adduser') }}</el-button>
+      <el-button type="primary" @click="handleDialogValue()">
+        {{ $t('table.adduser') }}
+      </el-button>
     </el-row>
     <el-table :data="tableData" stripe style="width: 100%">
       <el-table-column
@@ -22,13 +24,18 @@
         :key="index"
       >
         <template v-slot="{ row }" v-if="item.prop === 'mg_state'">
-          <el-switch v-model="row.mg_state" />
+          <el-switch v-model="row.mg_state" @change="changeState(row)" />
         </template>
         <template v-slot="{ row }" v-else-if="item.prop === 'create_time'">
           {{ $filters.filterTimes(row.create_time) }}
         </template>
         <template #default v-else-if="item.prop === 'action'">
-          <el-button type="primary" size="small" :icon="Edit"></el-button>
+          <el-button
+            type="primary"
+            size="small"
+            :icon="Edit"
+            @click="initGetUsersList(row)"
+          ></el-button>
           <el-button type="warning" size="small" :icon="Setting"></el-button>
           <el-button type="danger" size="small" :icon="Delete"></el-button>
         </template>
@@ -44,14 +51,26 @@
       @current-change="handleCurrentChange"
     />
   </el-card>
-  <div>users</div>
+  <Dialog
+    v-model="dialogVisible"
+    :dialogTitle="dialogTitle"
+    v-if="dialogVisible"
+    @initUserList="initGetUsersList"
+    :dialogTableValue="dialogTableValue"
+  />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { Search, Edit, Setting, Delete } from '@element-plus/icons-vue'
-import { getUser } from '@/apis/users'
+import { changeUserState, getUser, deleteUser } from '@/apis/users'
 import { options } from './options'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { useI18n } from 'vue-i18n'
+import Dialog from './components/dialog.vue'
+import { isNull } from '@/utils/filters'
+
+const i18n = useI18n()
 
 const queryForm = ref({
   query: '',
@@ -62,6 +81,10 @@ const queryForm = ref({
 const total = ref(0)
 
 const tableData = ref([])
+
+const dialogVisible = ref(false)
+const dialogTitle = ref('')
+const dialogTableValue = ref({})
 
 const initGetUsersList = async () => {
   const res = await getUser(queryForm.value)
@@ -80,6 +103,46 @@ const handleSizeChange = (pageSize) => {
 const handleCurrentChange = (pageNum) => {
   queryForm.value.pagenum = pageNum
   initGetUsersList()
+}
+
+const changeState = async (info) => {
+  await changeUserState(info.id, info.mg_state)
+  ElMessage({
+    message: i18n.t('message.updeteSuccess'),
+    type: 'success'
+  })
+}
+const handleDialogValue = (row) => {
+  console.log('handleDialogValue')
+  if (isNull(row)) {
+    dialogTitle.value = '添加用户'
+    dialogTableValue.value = {}
+  } else {
+    dialogTitle.value = '编辑用户'
+    dialogTableValue.value = JSON.parse(JSON.stringify(row))
+  }
+  dialogVisible.value = true
+}
+const delUser = (row) => {
+  ElMessageBox.confirm(i18n.t('dialog.deleteTitle'), 'Warning', {
+    confirmButtonText: 'OK',
+    cancelButtonText: 'Cancel',
+    type: 'warning'
+  })
+    .then(async () => {
+      await deleteUser(row.id)
+      ElMessage({
+        type: 'success',
+        message: 'Delete completed'
+      })
+      initGetUsersList()
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: 'Delete canceled'
+      })
+    })
 }
 </script>
 
